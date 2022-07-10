8f79ba007d3c314f18dece9873caf2778190944b
vhqtvn <vhqtvn@users.noreply.github.com>
2022-07-10 21:45:39 +0700

    initial

---
+++ GoodixTools/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace GoodixTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtHex = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Read Config";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtHex
+            // 
+            this.txtHex.Location = new System.Drawing.Point(13, 40);
+            this.txtHex.Multiline = true;
+            this.txtHex.Name = "txtHex";
+            this.txtHex.Size = new System.Drawing.Size(1133, 570);
+            this.txtHex.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 622);
+            this.Controls.Add(this.txtHex);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtHex;
+    }
+}
+
---
+++ GoodixTools/Form1.cs
@@ -0,0 +1,91 @@
+﻿using HidLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoodixTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        HidDevice device;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (device == null)
+            {
+                foreach (HidDevice hd in HidDevices.Enumerate())
+                {
+                    if (hd.ToString().Contains("gxt"))
+                    {
+                        Console.WriteLine(hd.ToString());
+                    }
+                }
+                foreach (HidDevice hd in HidDevices.Enumerate(0x27C6, 0x0113))
+                {
+                    if (hd.ToString().Contains("col03")) device = hd;
+                }
+            }
+            device.OpenDevice(DeviceMode.NonOverlapped, DeviceMode.NonOverlapped, ShareMode.ShareRead | ShareMode.ShareWrite);
+            if (!device.IsOpen)
+            {
+                Console.WriteLine("Not open");
+            }
+            Thread.Sleep(100);
+            byte[] buf = new byte[512];
+            for (int offset = 0x8050, xoffset = 0; xoffset < 500; offset += 50, xoffset += 50)
+            {
+                byte[] data = new byte[65];
+                int di = 0;
+                data[di++] = 0x0e;
+                data[di++] = 0x20;
+                data[di++] = 0x00;
+                data[di++] = 0x00;
+
+                data[di++] = 0x05;
+                data[di++] = 0x01;
+
+                data[di++] = (byte)(offset / 256);
+                data[di++] = (byte)(offset % 256);
+
+                data[di++] = 50;
+                data[di++] = 50;
+
+                Console.WriteLine("Send request");
+                HidReport wreport = new HidReport(65, new HidDeviceData(data, HidDeviceData.ReadStatus.Success));
+                for (int i = 0; i < 10; i++)
+                    if (!device.WriteReportSync(wreport))
+                    {
+                        Console.WriteLine("Failed");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ok");
+                        break;
+                    }
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(" Read request " + i + ": " + device.Capabilities.InputReportByteLength);
+                    HidReport report = device.ReadReportSync(0xe);
+                    if (report.ReadStatus == HidDeviceData.ReadStatus.NoDataRead) continue;
+                    Console.WriteLine("" + report.ReadStatus + " : " + BitConverter.ToString(report.Data));
+                    report.Data.Skip(4).Take(50).ToArray().CopyTo(buf, xoffset);
+                    break;
+                }
+            }
+            device.CloseDevice();
+            txtHex.Text = "0x" + BitConverter.ToString(buf.Take(444).ToArray()).Replace("-", ",0x");
+        }
+    }
+}
